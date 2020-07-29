867fa8b44573f482cc90447ffe72e38f8bf86912
AndreyRenz <renzhinan2001@gmail.com>
2020-07-30 02:58:31 +0300

    Abrakadabra

--- ConsoleApp1/DBUtils.cs
+++ ConsoleApp1/DBUtils.cs
@@ -11,7 +11,7 @@ namespace Tutorial.SqlConn
     {
         public static SqlConnection GetDBConnection()
         {
-            string datasource = @"tran-vmware\SQLEXPRESS";
+            string datasource = @"DESKTOP-GH3H7IM\SQLEXPRESS";
 
             string database = "simplehr";
             string username = "sa";
