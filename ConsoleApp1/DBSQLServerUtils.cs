b0c74a834203dd4dccc749df5167750e2b5a9afb
AndreyRenz <renzhinan2001@gmail.com>
2020-07-30 14:27:31 +0300

    Modifited separately

--- ConsoleApp1/DBSQLServerUtils.cs
+++ ConsoleApp1/DBSQLServerUtils.cs
@@ -12,11 +12,16 @@ namespace Tutorial.SqlConn
     {
 
         public static SqlConnection
-                 GetDBConnection(string datasource, string database, string username, string password)
+        GetDBConnection(string datasource, string database, string username, string password)
         {
-            //
-            // Data Source=TRAN-VMWARE\SQLEXPRESS;Initial Catalog=simplehr;Persist Security Info=True;User ID=sa;Password=12345
-            //
+
+            //Data Source = DESKTOP - GH3H7IM\SQLEXPRESS;
+            //Initial Catalog = test;
+            //Persist Security Info = True;
+            //User ID = sa;
+            //Password = 228228;
+
+
             string connString = @"Data Source=" + datasource + ";Initial Catalog="
                         + database + ";Persist Security Info=True;User ID=" + username + ";Password=" + password;
 
